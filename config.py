3e1ba46deb6bb93914b4273cede0513e99aef011
harshnu12035 <harshnu12035@gmail.com>
2026-04-06 16:12:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SESSION = os.getenv("SESSION", "YOUR_SESSION_STRING_HERE")
-BOOST_LEVEL = int(os.getenv("BOOST_LEVEL", "200"))  # 150-200 dB
+BOOST_LEVEL = int(os.getenv("BOOST_LEVEL", "180"))  # 150-200 dB
 API_ID = int(os.getenv("API_ID", "YOUR_API_ID"))
 API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
